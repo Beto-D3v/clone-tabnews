bdad03dd7a30782271bbd1e92f411183e6107e4a
Beto-D3v <robertomacedo3007@gmail.com>
2024-11-15 11:16:16 -0300

    adicionando novos scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação de um projeto do https://curso.dev para a realização de um sistema completo
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Feliz aniversário, meu amor! Sinto que quando estamos unidos, podemos superar tudo! Fico feliz de ter você ao meu lado e que compartilharemos muitos momentos felizes e conquistas, juntos!!!</h1>
+function Home() {
+  return (
+    <h1>
+      Feliz aniversário, meu amor! Sinto que quando estamos unidos, podemos
+      superar tudo! Fico feliz de ter você ao meu lado e que compartilharemos
+      muitos momentos felizes e conquistas, juntos!!!
+    </h1>
+  );
 }
 
 export default Home;
