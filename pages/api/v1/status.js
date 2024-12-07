8ec0a12ff4ffd7e23e1fe458a7d0544c59f592c2
Roberto_Macedo <96922328+Beto-D3v@users.noreply.github.com>
2024-12-07 21:58:50 +0000

    Adicionando novos comandos no

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,7 +1,7 @@
-import database from "../../../infra/database.js"
+import database from "infra/database.js"
 
 async function status(request,response){
-    var result = await database.query("SELECT 1 + 1 as sum;")
+    const result = await database.query('SELECT 1 + 1;')
     console.log(result.rows);
     response.status(200).json({chave:"Você conseguiu localizar a mensagem, parabéns!!!"});
 }
