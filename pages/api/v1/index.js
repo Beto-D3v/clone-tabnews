825859c0c8a065fcffb909d13f5b9e75a5a859b1
Roberto_Macedo <96922328+Beto-D3v@users.noreply.github.com>
2024-12-08 20:26:55 +0000

    finaliza o endpoint de status

---
+++ pages/api/v1/index.js
@@ -0,0 +1,29 @@
+import database from "infra/database.js"
+
+async function status(request,response){
+    const updateAt = new Date().toISOString();
+    
+    const databaseVersion = await database.query('SHOW server_version;');
+    const pgVersion = databaseVersion.rows[0].server_version;
+    
+    const resultMaxConnections = await database.query('SHOW max_connections;');
+    const maxConnections = resultMaxConnections.rows[0].max_connections;
+
+    const databaseName = "local_db";
+    const resultActiveConnections = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [databaseName]
+    });
+    const activeConnections = resultActiveConnections.rows[0].count;
+
+    response.status(200).json({
+        update_at : updateAt,
+        dependencies:{
+            database_version: pgVersion,
+            max_connections : parseInt(maxConnections),
+            active_connections : activeConnections,
+        }
+    });
+}
+
+export default status;
--- pages/api/v1/status.js
+++
@@ -1,9 +0,0 @@
-import database from "infra/database.js"
-
-async function status(request,response){
-    const result = await database.query('SELECT 1 + 1;')
-    console.log(result.rows);
-    response.status(200).json({chave:"Você conseguiu localizar a mensagem, parabéns!!!"});
-}
-
-export default status;
